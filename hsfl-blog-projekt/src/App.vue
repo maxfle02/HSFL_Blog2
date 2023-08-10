148e6c909d62ae5ff35a9b2135134a716c64ede2
VencedorParis <sergey.hellwig@stud.hs-flensburg.de>
2023-08-10 13:36:10 +0200

    Navbar

---
+++ hsfl-blog-projekt/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+   <div class="header">
+      <h1>HSFL-Blog</h1>
+      <p>Blog der Hochschule Flensburg</p>
+    </div>
+  
+  <nav>
+    <div>
+    <router-link to="/">Startseite</router-link> |
+    <router-link to="/profile">Mein Account</router-link> |
+    <router-link to="/bibliothek">Bibliothek</router-link> 
+    </div>
+    <router-link to="/logging">Log Out</router-link>
+
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  
+  display: flex; 
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  background-color: #bad0f5;
+  text-align: left;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #bf654e;
+}
+.header h1 {
+    font-size: 50px;
+    margin-bottom: 5px; 
+    margin-top: px;
+  }
+  .header {
+    margin-top: 0;
+    padding: 2px;
+    text-align: center;
+    background: white;
+    color: black;
+    
+  }
+  .header p {
+  margin-top: 0;
+ 
+}
+</style>
