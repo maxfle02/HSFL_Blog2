fe964f89cda28c9a226f0d27ac1e95b0b9194ff3
Sergey Hellwig <117166205+VencedorParis@users.noreply.github.com>
2023-08-10 17:30:36 +0200

    add Blogbutton (#16)

--- hsfl-blog-projekt/src/views/Startseite.vue
+++ hsfl-blog-projekt/src/views/Startseite.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <button @click="createNewPost">New Post</button> <!-- Neuer Button -->
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -13,6 +14,31 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  methods: {
+    createNewPost() {
+      // Code, um einen neuen Beitrag zu erstellen oder zur Beitragserstellungsseite zu navigieren
+      console.log('New Post Button was clicked!');
+      alert('Klappt')
+    }
   }
 }
 </script>
+<style>
+.home button {
+  position: absolute;
+  top: 200px;
+  left: 10px;
+  padding: 10px 15px;
+  background-color: #42b983; /* Farbe des Links */
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.home button:hover {
+  background-color: #357a65; /* Dunklere Farbe beim Überfahren */
+}
+
+</style>
