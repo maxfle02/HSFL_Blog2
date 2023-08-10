283bb0afe886926e2ce5edb44653f1e7f14010e0
Sergey Hellwig <117166205+VencedorParis@users.noreply.github.com>
2023-08-10 14:01:23 +0200

    F1 (#14)
    * Navbar
    
    * add Navbar

---
+++ hsfl-blog-projekt/hsfl-blog-projekt/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/UserProfile.vue')
+  },
+
+  {
+    path: '/bibliothek',
+    name: 'bibliothek',
+    component: () => import('../views/SavedPosts.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ hsfl-blog-projekt/hsfl-blog-projekt/src/views/UserProfile.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bibliothek">
+    <h1>Hier sind meine gespiecherte Blog-Einträge</h1>
+  </div>
+</template>
